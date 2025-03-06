3f678a31cc32442f4e5d10687a6ecfe2c06b304c
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2025-03-06 20:59:44 +0530

    modfied

--- server.js
+++ server.js
@@ -8,17 +8,31 @@ require("dotenv").config();
 
 const app = express();
 
-/*-------- Global Middlewares -------- */
+
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
-/*--------- Routes ---------- */
+
 app.use("/api/auth", authRoutes);
 app.use("/api/quiz", quizRoutes);
 
-/*------ Connect to Database First ,then server-----*/
-connectDB().then(() => {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/api/data", (req, res) => {
+  res.json({ message: "API is working!" });
 });
+
+
+app.use((req, res) => {
+  res.status(404).json({ error: "404: NOT_FOUND" });
+});
+
+// Connect to Database First, then Start Server
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
